3b4af9c306b70d5cbf4f0e848556934b580da21f
xeeraki <shafai86@gmail.com>
2017-03-22 23:23:52 +0100

    formated

--- Java/src/Rectangle.java
+++ Java/src/Rectangle.java
@@ -4,45 +4,44 @@ import java.util.Arrays;
  * Created by shafai on 2017-03-20.
  */
 public class Rectangle {
-    public static void main(String[] args){
-        Rectangle[] rect = {new Rectangle(2,3,"yellow"),
-                new Rectangle(3,5,"red"),
-                new Rectangle(1,1,"yellow"),
-                new Rectangle(2,2,"red"),
-                new Rectangle(3,4,"red"),
-                new Rectangle(2,8,"red"),
-                new Rectangle(3,7,"yellow")};
+    public static void main(String[] args) {
+        Rectangle[] rect = {new Rectangle(2, 3, "yellow"),
+                new Rectangle(3, 5, "red"),
+                new Rectangle(1, 1, "yellow"),
+                new Rectangle(2, 2, "red"),
+                new Rectangle(3, 4, "red"),
+                new Rectangle(2, 8, "red"),
+                new Rectangle(5, 2, "yellow")};
 
-        System.out.println("The smallest rectangle with yellow color is :" +minRectangle(rect));
-        Rectangle[] rectangles = selectRectangles(rect,"yellow");
+        System.out.println("The smallest rectangle with yellow color is :" + minRectangle(rect));
+        Rectangle[] rectangles = selectRectangles(rect, "yellow");
         System.out.println(Arrays.toString(rectangles));
     }
+
     // the lengths of the rectangle’s sides
-    private double    length;
-    private double    width;
+    private double length;
+    private double width;
 
     // the rectangle’s color
-    private String    color;
+    private String color;
 
-    public Rectangle (double length, double width, String color)
-    {
+    public Rectangle(double length, double width, String color) {
         this.length = length;
         this.width = width;
         this.color = color;
     }
 
-    public String getColor ()
-    {
-        return  color;
+    public String getColor() {
+        return color;
     }
 
-    public double area ()
-    {
+    public double area() {
         return length * width;
 
     }
-    public String toString(){
-        return "<"+this.length +", "+this.width+","+this.color+">";
+
+    public String toString() {
+        return "<" + this.length + ", " + this.width + "," + this.color + ">";
 
     }
 
@@ -57,37 +56,34 @@ public class Rectangle {
          to determine the smallest of the rectangles in the array that are yellow.
   */
 
-    public static Rectangle minRectangle(Rectangle[] rect){
+    public static Rectangle minRectangle(Rectangle[] rect) {
 
         Rectangle min = rect[0];
-        for(int i = 0; i <rect.length;i++){
-            if(rect[i].area() < min.area()) {
+        for (int i = 0; i < rect.length; i++) {
+            if (rect[i].area() < min.area()) {
                 min = rect[i];
             }
         }
-    return min;
+        return min;
 
     }
 
-    public static Rectangle[] selectRectangles(Rectangle[] rect,String color){
+    public static Rectangle[] selectRectangles(Rectangle[] rect, String color) {
         int countRect = 0;
-        for(int i = 0; i <rect.length; i++){
-            if(rect[i].getColor().equals(color))
+        for (int i = 0; i < rect.length; i++) {
+            if (rect[i].getColor().equals(color))
                 countRect++;
 
         }
 
         Rectangle[] Rect = new Rectangle[countRect];
-        int companionVar=0;
-        for(int j = 0; j <countRect; j++) {
-                Rect[companionVar] = rect[j];
-
-                companionVar++;
-
+        int companionVar = 0;
+        for (int j = 0; j < countRect; j++) {
+            Rect[companionVar] = rect[j];
+            companionVar++;
         }
         return Rect;
     }
-
 
 
 }
